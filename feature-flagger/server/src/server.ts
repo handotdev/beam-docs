7a02a019e1e6155a3f1428ee963f9bbd301d80d9
mernit <egottli2@binghamton.edu>
2022-11-06 18:54:48 -0500

    beam docs

--- feature-flagger/server/src/server.ts
+++
@@ -1,6 +0,0 @@
-import app from './app';
-const port = 3000;
-
-app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
-});
--- src/controllers/TestController.ts
+++
@@ -1,9 +0,0 @@
-import { BaseController } from "./BaseController";
-
-export class TestController extends BaseController {  
-  protected async executeImpl (): Promise<void | any> {
-
-    let message = 'ts-express-api-template';
-    return this.jsonResponse(200, message);
-  }
-}
